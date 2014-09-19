45556dfcc6c59c399c9a7564bd34280db28f4be1
Tristan Siegel <tristan.t.siegel@gmail.com>
2014-09-19 13:22:37 -0400

    fixes error in the more_jobs? method

--- models/ms_job_scraper.rb
+++ models/ms_job_scraper.rb
@@ -1,9 +1,10 @@
 class MsJobScraper
-
+  attr_reader :last_known_url_jid
   attr_accessor :current_url_jid, :nokogirified_job_posting, :csv_file
 
-  def initialize(beggining_url_jid)
+  def initialize(beggining_url_jid, last_known_url_jid=160500)
     @current_url_jid = beggining_url_jid
+    @last_known_url_jid = last_known_url_jid
     create_new_csv
   end
 
@@ -26,9 +27,9 @@ class MsJobScraper
         ]
         csv_file << job_info_arr 
         actual_opennings += 1 #extraneous
-        puts job_title #extraneous
+        puts "Current Job: #{job_title}" #extraneous
       end
-      puts current_url_jid #extraneous
+      puts "Current Job ID: #{current_url_jid}" #extraneous
       @current_url_jid += 1
       @nokogirified_job_posting = visit_job_posting
     end
@@ -38,17 +39,18 @@ class MsJobScraper
   private
 
   def create_new_csv
-    @csv_file = CSV.open("ms-job-listings#{DateTime.new}.csv", "wb")
+    @csv_file = CSV.open("ms-job-listings-#{Time.now}.csv", "wb")
     @csv_file << ["Title", "Category", "Division", "Product", "Country", "State", "City", "Description", "URL", "Job ID"]
   end
 
   def more_jobs?
-    nokogirified_job_posting.url ||
-      visit_job_posting(current_url_jid + 1).url ||
-        visit_job_posting(current_url_jid + 3).url ||
-          visit_job_posting(current_url_jid + 7).url || 
-            visit_job_posting(current_url_jid + 11).url ||
-              visit_job_posting(current_url_jid + 13).url
+    current_url_jid <= last_known_url_jid ||
+      nokogirified_job_posting.url ||
+        visit_job_posting(current_url_jid + 1).url ||
+          visit_job_posting(current_url_jid + 3).url ||
+            visit_job_posting(current_url_jid + 7).url || 
+              visit_job_posting(current_url_jid + 11).url ||
+                visit_job_posting(current_url_jid + 13).url
   end
 
   def job_we_want?
@@ -70,7 +72,7 @@ class MsJobScraper
   def nokogiri_job_detail_text_selector(css_id_sub_text)
     node_arr = nokogirified_job_posting.css("span#ctl00_ContentPlaceHolder1_JobDetails2_lbl#{css_id_sub_text}Text")
     if !node_arr.empty?
-      nokogirified_job_posting.css("span#ctl00_ContentPlaceHolder1_JobDetails2_lbl#{css_id_sub_text}Text").text
+      node_arr.text
     else
       "N/A"
     end
--- runner.rb
+++ runner.rb
@@ -1,4 +1,4 @@
 require_relative 'config/environment.rb'
 
-scraper = MsJobScraper.new(160500)
+scraper = MsJobScraper.new(143100)
 scraper.scrape
